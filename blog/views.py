f662a2e20fb85d0a6928d72a0ecacd7e76623d52
Thakur Bhanu <onlyforworkandstuff@gmail.com>
2020-02-05 16:36:07 +0530

    sync

--- blog/views.py
+++ blog/views.py
@@ -4,21 +4,27 @@ from django.shortcuts import render
 
 posts = [
     {
-        'author' : 'CoreyMS',
-        'title' : 'Blog Post 1',
-        'content' : 'First Post content',
-        'date_posted' : 'August 27, 2018'
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First Post content',
+        'date_posted': 'August 27, 2018'
     },
     {
-        'author' : 'CoreyMS',
-        'title' : 'Blog Post 1',
-        'content' : 'First Post content',
-        'date_posted' : 'August 27, 2018'
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First Post content',
+        'date_posted': 'August 27, 2018'
     }
 ]
 
+
+
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
