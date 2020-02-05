4813e0b51e90cb636c9a13daf4bd6b0f0100444a
Thakur Bhanu <onlyforworkandstuff@gmail.com>
2020-02-05 16:09:35 +0530

    sync

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Blog Home </h1>')
+    return render(request, 'blog/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>Blog about </h1>')
+    return render(request, 'blog/about.html')
