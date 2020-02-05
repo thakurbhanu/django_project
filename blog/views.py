842a955584c4555ed5ce046eea0187bafdfeddf4
Thakur Bhanu <onlyforworkandstuff@gmail.com>
2020-02-05 16:29:07 +0530

    sync

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+#dummy data
+
+posts = [
+    {
+        'author' : 'CoreyMS',
+        'title' : 'Blog Post 1',
+        'content' : 'First Post content',
+        'date_posted' : 'August 27, 2018'
+    },
+    {
+        'author' : 'CoreyMS',
+        'title' : 'Blog Post 1',
+        'content' : 'First Post content',
+        'date_posted' : 'August 27, 2018'
+    }
+]
 
 def home(request):
     return render(request, 'blog/home.html')
