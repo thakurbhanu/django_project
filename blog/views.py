be0451326b7c715dc6e6cc566227ed9b065f4907
Thakur Bhanu <onlyforworkandstuff@gmail.com>
2020-02-06 18:55:23 +0530

    sync

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,5 @@
 from django.shortcuts import render
 from .models import Post
-#dummy data
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First Post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 2',
-        'content': 'Second Post content',
-        'date_posted': 'August 29, 2018'
-    }
-]
-
-
-
-
 
 def home(request):
 
